9be06215a4c95675bcb3c832a146e52ff7178d6d
Dennis Stumm <ds695sas@gmail.com>
2025-05-02 12:35:55 +0200

    Fixed regex

--- generate.ts
+++ generate.ts
@@ -40,10 +40,10 @@ interface Asset {
 }
 
 const ASSET_PATTERNS: [Asset["os"], Asset["arch"], RegExp][] = [
-  ["darwin", "arm64", /^(clang\+llvm-.+-arm64-apple-(darwin|macos).*)|(LLVM-.*-macOS-ARM64)\.tar\.xz$/],
-  ["darwin", "x64", /^(clang\+llvm-.+-x86_64-apple-(darwin|macos).*)|(LLVM-.*-macOS-X64)\.tar\.xz$/],
-  ["linux", "arm64", /^(clang\+llvm-.+-aarch64-linux-gnu.*)|(LLVM-.+-Linux-ARM64)\.tar\.xz$/],
-  ["linux", "x64", /^(clang\+llvm-.+-x86_64-linux-gnu-?ubuntu.*)|(LLVM-.+-Linux-X64)\.tar\.xz$/],
+  ["darwin", "arm64", /^((clang\+llvm-.+-arm64-apple-(darwin|macos).*)|(LLVM-.*-macOS-ARM64))\.tar\.xz$/],
+  ["darwin", "x64", /^((clang\+llvm-.+-x86_64-apple-(darwin|macos).*)|(LLVM-.*-macOS-X64))\.tar\.xz$/],
+  ["linux", "arm64", /^((clang\+llvm-.+-aarch64-linux-gnu.*)|(LLVM-.+-Linux-ARM64))\.tar\.xz$/],
+  ["linux", "x64", /^((clang\+llvm-.+-x86_64-linux-gnu-?ubuntu.*)|(LLVM-.+-Linux-X64))\.tar\.xz$/],
   ["win32", "arm64", /^LLVM-.+-woa64\.exe$/],
   ["win32", "x64", /^LLVM-.+-win64\.exe$/],
 ];
